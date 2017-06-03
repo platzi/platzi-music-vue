fe71c6d7c42516a16f36f6a620dbdb71a2d7a8cb
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-06-03 00:54:41 -0700

    Cleaning up

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,17 @@
 <template lang="pug">
   main#app
     pm-header
-    pm-container
     pm-footer
 </template>
 
 <script>
   import PmHeader from './components/layout/Header.vue'
   import PmFooter from './components/layout/Footer.vue'
-  import PmContainer from './components/layout/Container.vue'
 
   export default {
     name: 'app',
 
-    components: { PmHeader, PmFooter, PmContainer },
-
-    data () {
-      return {
-        title: '🎵 Platzi Music'
-      }
-    }
+    components: { PmHeader, PmFooter }
   }
 </script>
 
