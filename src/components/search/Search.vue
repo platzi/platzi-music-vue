c9092c6c38319ffeb234a13fda9f83af454bb042
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-06-03 01:24:05 -0700

    Adding v-on, methods and modifiers

--- src/components/search/Search.vue
+++ src/components/search/Search.vue
@@ -3,10 +3,15 @@
     .container
       nav.nav.has-shadow
        .container
+          a.is-reset(@click.prevent="reset") X
           input.input.is-large(type="text" placeholder="Busca una cancion", v-model="searchQuery")
+          button.button.is-info.is-large(@click="search") Buscar
       .container
         p(v-show="searchQuery")
           small {{ queryMessage }}
+      .container
+        .columns(v-show="tracks.length")
+          .column(v-for="r in tracks") {{ r }}
 </template>
 
 <script>
@@ -14,13 +19,25 @@ export default {
 
   data () {
     return {
-      searchQuery: ''
+      searchQuery: '',
+      tracks: []
     }
   },
 
   computed: {
     queryMessage () {
       return `Buscando resultados para: ${this.searchQuery}`
+    }
+  },
+
+  methods: {
+    search () {
+      this.tracks = [1, 2, 3]
+    },
+
+    reset () {
+      this.searchQuery = ''
+      this.tracks = []
     }
   }
 }
