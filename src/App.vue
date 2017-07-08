aebe8dd42ffc19a2ecafea805cdc1fa8d545a09b
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-07-08 01:23:21 -0300

    Adding dir samples

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template lang="pug">
   #app
-    h1 {{ msg }}
-    p {{ 1 + 1 }}
-    p {{ 'Hola' + 'Mundo' }}
-    p {{ person.name }}
-    p {{ person.name.toUpperCase() }}
-    p {{ JSON.stringify(person) }}
-    p {{ true ? 'true' : 'false' }}
+    p(v-show="showValue") {{ value }}
+    p(v-if="showValue") {{ value }}
+    p(v-else-if="false") {{ 'algo mas' }}
+    p(v-else) {{ 'lo ultimo' }}
+
+    ul
+      li(v-for="i in items") {{ i }}
 </template>
 
 <script>
@@ -16,9 +16,10 @@ export default {
     return {
       msg: 'Hola Vue!',
 
-      person: {
-        name: 'Juan'
-      }
+      showValue: false,
+      value: 'Algo',
+
+      items: [1, 2, 3, 4, 5]
     }
   }
 }
