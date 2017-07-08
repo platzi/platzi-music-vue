3e4940b6a44b3e640e3a3c2c10b4d963ddafd330
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-07-08 17:05:04 -0300

    Adding track service

---
+++ src/services/track.js
@@ -0,0 +1,14 @@
+import platziMusicService from './platzi-music-service'
+
+const trackService = {}
+
+trackService.search = function (q) {
+  const type = 'track'
+
+  return platziMusicService.get('/search', {
+    params: { q, type }
+  })
+    .then(res => res.data)
+}
+
+export default trackService
