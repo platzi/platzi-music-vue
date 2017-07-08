25902c2420d7ea1160e6a2107f59110b439c2b5e
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-07-08 00:49:05 -0300

    Setting info

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,13 @@ var webpack = require('webpack')
 
 module.exports = {
   entry: './src/main.js',
+
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
     filename: 'build.js'
   },
+
   module: {
     rules: [
       {
@@ -15,13 +17,9 @@ module.exports = {
         loader: 'vue-loader',
         options: {
           loaders: {
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
-            // other preprocessors should work out of the box, no loader config like this necessary.
             'scss': 'vue-style-loader!css-loader!sass-loader',
             'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
           }
-          // other vue-loader options go here
         }
       },
       {
@@ -45,7 +43,7 @@ module.exports = {
   },
   devServer: {
     historyApiFallback: true,
-    noInfo: true
+    noInfo: false
   },
   performance: {
     hints: false
