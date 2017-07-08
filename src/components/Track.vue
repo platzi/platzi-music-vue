8569679160ea3637817e35301763d14ebcfdb126
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-07-08 17:39:41 -0300

    Adding track component

---
+++ src/components/Track.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  .card
+    .card-image
+      figure.image.is-1by1
+        img()
+
+    .card-content
+      .media
+        .media-left
+          figure.image.is-48x48
+        .media-content
+          p.title.is-6
+            strong
+          p.subtitle.is-6
+
+        .content
+          small
+          nav.level
+            .level-left
+              a.level-item
+                span.icon.is-small ▶️
+</template>
+
+<script>
+export default {
+  props: {
+    track: { type: Object, required: true }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
