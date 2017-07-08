2a9a1b732acf0dc8ef75857f3bc4b18ebce22559
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-07-08 01:48:56 -0300

    Adding computed and v-for

--- src/App.vue
+++ src/App.vue
@@ -10,30 +10,49 @@
           )
           a.button.is-info.is-large(@click="search") Buscar
           a.button.is-danger.is-large &times;
+          p
+            small {{ searchMessage }}
 
-      .container
+      .container.results
         .columns
-          .column
+          .column(v-for="t in tracks") {{ t.name }} - {{ t.artist}}
 </template>
 
 <script>
+const tracks = [
+  { name: 'Muchacha', artist: 'Luis Alberto Spinetta' },
+  { name: 'Hoy aca en el baile', artist: 'El Pepo' },
+  { name: 'I was made for loving you', artist: 'Kiss' }
+]
+
 export default {
   name: 'app',
 
   data () {
     return {
-      searchQuery: ''
+      searchQuery: '',
+      tracks: []
+    }
+  },
+
+  computed: {
+    searchMessage () {
+      return `Encontrados: ${this.tracks.length}`
     }
   },
 
   methods: {
     search () {
-      console.log(this.searchQuery)
+      this.tracks = tracks
     }
   }
 }
 </script>
 
 <style lang="scss">
-  @import './scss/main.scss'
+  @import './scss/main.scss';
+
+  .results {
+    margin-top: 50px;
+  }
 </style>
