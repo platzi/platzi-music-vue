422e12fa5fcf9c47cb1fe2483fce54a98545b4c8
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-07-08 01:31:57 -0300

    Adding watcher sample

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,12 @@ export default {
     fullName () {
       return `${this.name} ${this.lastName}`
     }
+  },
+
+  watch: {
+    name (newVal, oldVal) {
+      console.log(newVal, oldVal)
+    }
   }
 }
 </script>
