1d34c5821aebcf56da4cb575fa9ccf60c4fc78ce
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-07-08 01:13:49 -0300

    Replacing HTML by pug

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-  </div>
+<template lang="pug">
+  #app
+    img(src="./assets/logo.png")
+    h1 {{ msg }}
 </template>
 
 <script>
