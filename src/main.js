41327b69d39c9dc23ead491a5371bd88b03ce061
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-07-09 23:47:05 -0300

    Adding routes

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from '@/App.vue'
+
+import routes from '@/routes'
 
 import EventBus from '@/plugins/event-bus'
 
+Vue.use(VueRouter)
 Vue.use(EventBus)
+
+const router = new VueRouter({ routes })
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router
 })
---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import Search from '@/components/Search.vue'
+
+const routes = [
+  { path: '/', component: Search, name: 'search' }
+]
+
+export default routes
