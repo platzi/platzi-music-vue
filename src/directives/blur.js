2ff60894d8fb2acf0c3c7c6646198736ffd14762
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-07-10 11:24:09 -0300

    Adding blur dir

---
+++ src/directives/blur.js
@@ -0,0 +1,24 @@
+const blur = {}
+
+function setBlur (el, binding) {
+  el.style.filter = !binding.value ? 'blur(3px)' : '(none)'
+  el.style.cursos = !binding.value ? 'not-allowed' : 'inherit'
+
+  el.querySelectorAll('a').forEach(a => {
+    if (!binding.value) {
+      a.setAttribute('disabled', true)
+    } else {
+      a.removeAttribute('disabled')
+    }
+  })
+}
+
+blur.install = function (Vue) {
+  Vue.directive('blur', {
+    bind (el, binding) {
+      setBlur(el, binding)
+    }
+  })
+}
+
+export default blur
