96648ac2d0dba45bd753d41aa7e49b01928e151a
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-07-09 23:57:23 -0300

    Adding menu

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -7,6 +7,8 @@
             .nav-item
               strong 📻  Platzi Music
             .nav-right.nav-menu
+              router-link.nav-item(:to="{ name: 'search' }") Buscar
+              router-link.nav-item(to="about") Nosotros
 
     .hero-body
       .container.has-text-centered
